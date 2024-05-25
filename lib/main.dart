7cf172331f90ee2c1f1410aed17e0b3df9ece926
Procodersuo <100094332+Procodersuo@users.noreply.github.com>
2024-05-25 22:47:42 +0500

    authentication folder added

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -20,7 +17,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 

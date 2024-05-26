8e973fedb78b49948bb4dcd5b3bda4cd9cb21e05
Procodersuo <100094332+Procodersuo@users.noreply.github.com>
2024-05-26 10:14:41 +0500

    Authentication folder Created

--- lib/Authentication/loginscreen.dart
+++ lib/Authentication/loginscreen.dart
@@ -4,6 +4,8 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      // body: ,
+    );
   }
 }

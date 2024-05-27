39ce64261afd2802dbf29e323fe083a2578451c8
Procodersuo <100094332+Procodersuo@users.noreply.github.com>
2024-05-27 13:17:29 +0500

    Constant Folder Added

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class AppConstant {
+  static String appName="Log In";
+  static  const loginButtonColor=Color(0xFFfff1ba);
+  static const textFormField=Color(0xFFffffff);
+
+}

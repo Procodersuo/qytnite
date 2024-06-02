5e56018e50c4c43ca2d0ae0ac46f821a1339eddb
Talha Shoaib <100094332+Procodersuo@users.noreply.github.com>
2024-06-02 21:44:52 +0500

    Update signup_signin_controller.dart

--- lib/services/signup_signin_controller.dart
+++ lib/services/signup_signin_controller.dart
@@ -2,9 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
-
-
   Future<void> signUp(BuildContext context, String email, String password) async {
 
     try {

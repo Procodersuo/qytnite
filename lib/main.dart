d59d0a8dd697cd5e65c0ef6cf559b2db860707a9
Procodersuo <100094332+Procodersuo@users.noreply.github.com>
2024-06-05 19:10:56 +0500

    Text Field Widget added and All colors added now see text widget and use it accordingly

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qytnite/firebase_options.dart';
 void main() async {
@@ -12,10 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return const MaterialApp(
 
     );
   }
+
 }
 
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
 class MyAppColors {
-
-  static  const loginButtonColor=Color(0xFFfff1ba);
-  static const textFormField=Color(0xFFffffff);
+  static const primaryYellow = Color(0xFFFFF1BA);
+  static const secondaryBlue = Color(0xFFC8DEF9);
+  static const white = Color(0xFFFFFFFF);
+  static const lightGray = Color(0xFFDDDFE3);
+  static const darkText = Color(0xFF00008B);
+  static const lightestGray = Color(0xFFE5E7EB);
+  static const darkGray = Color(0xFFADACAC);
+  static const containerGray = Color(0xFFBFBFBF);
+  static const containerYellow = Color(0xFFF6D656);
+  static const successGreen = Color(0xFF1EE21A);
+  static const deviceYellow = Color(0xFFFEEA3C);
+  static const deviceMaroon = Color(0xFF9E637D);
+  static const deviceList = Color(0xFFF9F3BC);
+  static const lightYellow = Color(0xFFFFF1BA);
+  static const failureRed = Color(0xFFED3F19);
+  static const dialogDescription = Color(0xFFB0B0DB);
+  static const secondaryPink = Color(0xFFF9C3B8);
   //Gradients
   static const LinearGradient primaryGradient = LinearGradient(
     begin: Alignment.topCenter,
